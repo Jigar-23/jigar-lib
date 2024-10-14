e8f77b9c7f52cd59c3c6d956a32709c98f5d9c90
Jigar-23 <104854418+Jigar-23@users.noreply.github.com>
2024-10-14 21:54:18 +0530

    gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    `maven-publish` // Add this line to apply the maven-publish plugin
 }
 
 group = "com.github.Jigar-23"
@@ -18,3 +19,44 @@ tasks.test {
     useJUnitPlatform()
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            // Define your POM information
+            pom {
+                name.set("jigar-lib")
+                description.set("This is a library of mine")
+                url.set("https://github.com/Jigar-23/jigar-lib")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Jigar-23")
+                        name.set("Jigar")
+                        email.set("Pulkit10112@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Jigar-23/jigar-lib.git")
+                    developerConnection.set("scm:git:ssh://github.com/Jigar-23/jigar-lib.git")
+                    url.set("https://github.com/Jigar-23/jigar-lib/")
+                }
+
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://jitpack.io")
+            credentials {
+                username = "Jigar-23"  // Your GitHub username
+                password = "jp_auvmfu70aj57gvbo5nmraeh8i4"  // Your JitPack access token
+            }
+        }
+    }
+}
